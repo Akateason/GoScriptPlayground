556c5d61bd0c49577fb9eb694bd535c69ce87eea
Mamba24 <akateason@qq.com>
2022-08-21 11:29:43 +0800

    [feat] workspace

--- goScript/boom/boom.go
+++ goScript/boom/boom.go
@@ -1,93 +1,93 @@
 package main
 
-import (	
-	"fmt"	
-    "log"
-    "os"
-	"os/exec"		
-	"sync"
+import (
 	"bufio"
-    "io"
-    // "sort"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
 
-    "github.com/urfave/cli/v2"
+	// "sort"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-    // app := &cli.App{
-    //     Name:  "boom",
-    //     Usage: "make an explosive entrance",
-    //     Action: func(ctx *cli.Context) error {
+	// app := &cli.App{
+	//     Name:  "boom",
+	//     Usage: "make an explosive entrance",
+	//     Action: func(ctx *cli.Context) error {
 
-    //         // Arguments 参数  
-    //         fmt.Printf("step1 boom! I say %q \n", ctx.Args().Get(0)) 
+	//         // Arguments 参数
+	//         fmt.Printf("step1 boom! I say %q \n", ctx.Args().Get(0))
 
-    //         // Command("ls -l")
-    //         // Command("cd a; ls") // cd成功了
-    //         // fmt.Println("step2 22222222")
-    //         // Command("ls")
+	//         // Command("ls -l")
+	//         // Command("cd a; ls") // cd成功了
+	//         // fmt.Println("step2 22222222")
+	//         // Command("ls")
 
-    //         return nil
-    //     },
-    // }
+	//         return nil
+	//     },
+	// }
 
+	app := &cli.App{
+		EnableBashCompletion: true,
 
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "add a task to the list",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("added task: ", cCtx.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:    "complete",
+				Aliases: []string{"c"},
+				Usage:   "complete a task on the list",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("completed task: ", cCtx.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:    "template",
+				Aliases: []string{"t"},
+				Usage:   "options for task templates",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "add",
+						Usage: "add a new template",
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("new task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+					{
+						Name:  "remove",
+						Usage: "remove an existing template",
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("removed task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+				},
+			},
+		},
+	}
 
-
-    app := &cli.App{
-        Commands: []*cli.Command{
-            {
-                Name:    "add",
-                Aliases: []string{"a"},
-                Usage:   "add a task to the list",
-                Action: func(cCtx *cli.Context) error {
-                    fmt.Println("added task: ", cCtx.Args().First())
-                    return nil
-                },
-            },
-            {
-                Name:    "complete",
-                Aliases: []string{"c"},
-                Usage:   "complete a task on the list",
-                Action: func(cCtx *cli.Context) error {
-                    fmt.Println("completed task: ", cCtx.Args().First())
-                    return nil
-                },
-            },
-            {
-                Name:    "template",
-                Aliases: []string{"t"},
-                Usage:   "options for task templates",
-                Subcommands: []*cli.Command{
-                    {
-                        Name:  "add",
-                        Usage: "add a new template",
-                        Action: func(cCtx *cli.Context) error {
-                            fmt.Println("new task template: ", cCtx.Args().First())
-                            return nil
-                        },
-                    },
-                    {
-                        Name:  "remove",
-                        Usage: "remove an existing template",
-                        Action: func(cCtx *cli.Context) error {
-                            fmt.Println("removed task template: ", cCtx.Args().First())
-                            return nil
-                        },
-                    },
-                },
-            },
-        },
-    }
-
-    if err := app.Run(os.Args); err != nil {
-        log.Fatal(err)
-    }
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // go调用shell
 func Command(cmd string) error {
-	c := exec.Command("bash", "-c", cmd)  // mac or linux
+	c := exec.Command("bash", "-c", cmd) // mac or linux
 	stdout, err := c.StdoutPipe()
 	if err != nil {
 		return err
