3da6da9eaf2c2e0847ab167b784e52a4c2b50853
Mamba24 <akateason@qq.com>
2022-10-08 01:13:06 +0800

    rename

--- goScript/podV/podV.go
+++ goScript/podV/podV.go
@@ -11,13 +11,13 @@ import (
 
 func main() {
 	app := &cli.App{
-		Name:  "checkPod",
-		Usage: "podfile.lock Analysis之后. 搜索pod版本号..",
+		Name:  "podV",
+		Usage: "找pod版本号",
 		Action: func(ctx *cli.Context) error {
-			fmt.Printf("查pod版本, 输入参数: %q \n", ctx.Args()) // Arguments 参数
+			fmt.Printf("查pod版本, 输入的参数为: %q \n", ctx.Args()) // Arguments 参数
 			paramSearch := ctx.Args().Get(0)
 			if paramSearch == "" {
-				fmt.Printf("你没输入参数, 我挂了 \n")
+				fmt.Printf("❎ 你没有输入参数, 输入要查的pod \n")
 				return nil
 			}
 
