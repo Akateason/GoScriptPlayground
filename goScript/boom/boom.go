32c0bd6c2964cfe98bebfa852d01f5e43f968ee6
tianchen.xie <tianchen.xie@nio.com>
2024-02-22 16:33:55 +0800

    chore: clear boom

--- goScript/boom/boom.go
+++ goScript/boom/boom.go
@@ -12,10 +12,8 @@ package main
 
 import (
 	"fmt"
-	"goPlay/earth"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/urfave/cli/v2"
 )
@@ -30,29 +28,7 @@ func main() {
 			// get Arguments 参数
 			// fmt.Printf("单元测试~~~args === %q\n", ctx.Args())
 			// fmt.Printf("boom! I say %q \n", ctx.Args().Get(0))
-			fmt.Println("🚀auto reform view to cell")
-
-			// get cell template first
-			template := earth.ReadFileFrom("src/XXXCell.swift.tmp")
-			// fmt.Printf(template)
-
-			var files []string
-			files, _ = earth.GetAllFilePaths("input", files)
-			for i := 0; i < len(files); i++ {
-				fileName := files[i]
-				if strings.HasSuffix(fileName, ".swift") {
-					// get customView's name
-					fileName = strings.Replace(fileName, ".swift", "", -1)
-					fileName = strings.Replace(fileName, "input/", "", -1)
-					fmt.Println(fileName)
-
-					// generate newfile
-					content := strings.Replace(template, "XXX", fileName, -1)
-					newFileName := "output/" + fileName + "Cell.swift"
-					earth.WriteStringToFileFrom(newFileName, content)
-
-				}
-			}
+			fmt.Println("🚀")
 
 			// fmt.Printf(result)
 
